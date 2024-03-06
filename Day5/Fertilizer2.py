fce11e7d7b3477d02e7d3ada932f740d312eb7f0
guillezuhe <guillezuhe@gmail.com>
2024-03-06 02:27:22 +0100

    Day 5 with part 2 brute-force

---
+++ Day5/Fertilizer2.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+# Open the input file
+file = open("Day5/input.txt", "r")
+
+# Read the file
+data = file.readlines()
+
+#print(data)
+
+# The first line of the input file contains the seed ranges
+seeds_builder = data[0].strip().split(":")[1].strip().split(" ")
+seeds_builder = [int(x) for x in seeds_builder]
+
+
+
+# Read the maps
+# Each map is separated by a blank line and has a title line
+maps = []
+map = []
+for line in data[2:]:
+    if line.strip() == "":
+        maps.append(map)
+        map = []
+    # Check if we are at the last line
+    elif data.index(line) == len(data)-1:
+        map.append([int(x) for x in line.strip().split('\n')[0].strip().split(" ")])
+        maps.append(map)
+    # Check if the line is a title
+    elif line.strip().split(" ")[0].isdigit() == False:
+        # It is a title, so we skip it
+        continue
+    else:
+        map.append([int(x) for x in line.strip().split('\n')[0].strip().split(" ")])
+
+
+# Now we have the seeds and the maps and we can start the transformation process
+#mapped = seeds
+min_mapped = np.inf
+
+
+for i in range(0, len(seeds_builder), 2):
+    print('Original seed', seeds_builder[i])
+    for j in range(0, seeds_builder[i+1]+1):
+        mapped = seeds_builder[i] + j
+        for map in maps:
+            for range_ in map:
+                # Get the source range start
+                source_start = range_[1]
+                destination_start = range_[0]
+                range_len = range_[2]
+
+                if mapped >= source_start and mapped < (source_start + range_len):
+                    mapped = destination_start + (mapped - source_start)
+                    break
+        if mapped < min_mapped:
+            min_mapped = mapped
+
+    #print('Mapped', mapped)
+
+# Look for the minimum of the last mapped values
+print('Minimum', min_mapped)
